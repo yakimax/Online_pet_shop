a626ccea5fa645de4ee44aa7b35f3dd11d71e65e
Yakimax <sv1099912@gmail.com>
2024-02-21 12:03:20 +0530

    add

--- online-pet-shop/src/NavbarComponent/SellerHeader.jsx
+++ online-pet-shop/src/NavbarComponent/SellerHeader.jsx
@@ -18,62 +18,79 @@ const SellerHeader = () => {
       draggable: true,
       progress: undefined,
     });
-    sessionStorage.removeItem("active-seller") ;
-    sessionStorage.removeItem("seller-jwtToken") ;
-    window.location.reload(true) ;
+    sessionStorage.removeItem("active-seller");
+    sessionStorage.removeItem("seller-jwtToken");
     setTimeout(() => {
-      navigate("/home") ;
+      navigate("/home");
+      window.location.reload(true);
     }, 2000); // Redirect after 3 seconds
-  } ;
+  };
+
   return (
-    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-5">
-      <li class="nav-item">
-        <Link
-          to="/seller/order/all"
-          class="nav-link active"
-          aria-current="page"
+    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 me-5">
+      <li className="nav-item dropdown">
+        <a
+          className="nav-link dropdown-toggle"
+          href="#"
+          id="navbarDropdown"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
         >
-          <b className="text-color">Seller Orders</b>
-        </Link>
+          <b className="text-color">Seller Menu</b>
+        </a>
+        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li>
+            <Link
+              to="/seller/order/all"
+              className="dropdown-item"
+              aria-current="page"
+            >
+              Seller Orders
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/seller/delivery/register"
+              className="dropdown-item"
+              aria-current="page"
+            >
+              Register Delivery
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/seller/delivery-person/all"
+              className="dropdown-item"
+              aria-current="page"
+            >
+              View Delivery Persons
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/product/add"
+              className="dropdown-item"
+              aria-current="page"
+            >
+              Add Product
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/seller/product/all"
+              className="dropdown-item"
+              aria-current="page"
+            >
+              View My Products
+            </Link>
+          </li>
+        </ul>
       </li>
-      <li class="nav-item">
-        <Link
-          to="/seller/delivery/register"
-          class="nav-link active"
-          aria-current="page"
-        >
-          <b className="text-color">Register Delivery</b>
-        </Link>
-      </li>
-      <li class="nav-item">
-        <Link
-          to="/seller/delivery-person/all"
-          class="nav-link active"
-          aria-current="page"
-        >
-          <b className="text-color">View Delivery Persons</b>
-        </Link>
-      </li>
-      <li class="nav-item">
-        <Link to="/product/add" class="nav-link active" aria-current="page">
-          <b className="text-color">Add Product</b>
-        </Link>
-      </li>
-
-      <li class="nav-item">
-        <Link
-          to="/seller/product/all"
-          class="nav-link active"
-          aria-current="page"
-        >
-          <b className="text-color">View My Products</b>
-        </Link>
-      </li>
-
-      <li class="nav-item">
+      <li className="nav-item">
         <Link
           to=""
-          class="nav-link active"
+          className="nav-link active"
           aria-current="page"
           onClick={sellerLogout}
         >
