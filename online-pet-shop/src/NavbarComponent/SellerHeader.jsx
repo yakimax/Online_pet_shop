67b823c12a41845e087acffafe6dccba4689b200
Yakimax <sv1099912@gmail.com>
2024-02-21 12:08:21 +0530

    add

--- online-pet-shop/src/NavbarComponent/SellerHeader.jsx
+++ online-pet-shop/src/NavbarComponent/SellerHeader.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom" ;
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
